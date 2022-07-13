28ffad1688bb864ba78134ad27906dff001126c3
Hazel Tan <e0424860@u.nus.edu>
2022-07-13 17:03:57 +0800

    Add source code link at footer

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -1,10 +1,30 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Button, Flex } from '@chakra-ui/react';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 
 const Footer = () => {
   return (
-    <Box paddingY={4} align="center" opacity={0.4} fontSize="sm">
-      &copy; {new Date().getFullYear()} Hazel Hedmine Tan. All Rights Reserved.
-    </Box>
+    <Flex justify="center" direction="column">
+      <Box align="center">
+        <Button
+          size="sm"
+          fontSize="sm"
+          aria-label={`Go to Repository`}
+          variant="solid"
+          colorScheme="coral"
+          marginLeft="2"
+          onClick={() => {
+            window.open('https://github.com/hazelhedmine/portfolio-website');
+          }}
+          leftIcon={<ExternalLinkIcon />}
+        >
+          Source Code
+        </Button>
+      </Box>
+      <Box paddingY={4} align="center" opacity={0.4} fontSize="sm">
+        &copy; {new Date().getFullYear()} Hazel Hedmine Tan. All Rights
+        Reserved.
+      </Box>
+    </Flex>
   );
 };
 
