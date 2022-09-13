ec02ea831da18679a3318f79b8578d8ab01dde52
Hazel Tan <e0424860@u.nus.edu>
2022-09-14 00:21:06 +0800

    Add TA job to work experience

--- src/components/sections/Work.js
+++ src/components/sections/Work.js
@@ -1,5 +1,6 @@
 import {
   Box,
+  Divider,
   Flex,
   ListItem,
   Stat,
@@ -14,6 +15,42 @@ import Transition from '../transition';
 const Work = ({ props }) => {
   return (
     <Transition>
+      <Flex direction="column" p={6} gap={6}>
+        <Stat textAlign={{ base: 'center', md: 'left' }}>
+          <StatLabel>
+            National University of Singapore, School of Computing
+          </StatLabel>
+          <StatNumber>Teaching Assistant</StatNumber>
+          <StatHelpText>August 2022 - Present</StatHelpText>
+        </Stat>
+        <Flex gap={4} flexWrap="wrap" justify={{ base: 'center', md: 'left' }}>
+          <Tag colorScheme="cyan">Unity</Tag>
+          <Tag colorScheme="cyan">Vuforia Engine</Tag>
+          <Tag colorScheme="cyan">C#</Tag>
+          <Tag colorScheme="cyan">AMQP</Tag>
+        </Flex>
+        <Box align="left">
+          <UnorderedList fontSize="sm">
+            <ListItem>
+              Facilitated the computer engineering capstone project requiring
+              students to set up an AR laser tag system.
+            </ListItem>
+            <ListItem>
+              Mentored 3 groups of 5 students and refined project requirements
+              alongside professors.
+            </ListItem>
+            <ListItem>
+              Served as the primary teaching assistant for the software
+              visualizer component.
+            </ListItem>
+          </UnorderedList>
+        </Box>
+      </Flex>
+
+      <Flex paddingX={6}>
+        <Divider borderBottomColor="cyan.300"></Divider>
+      </Flex>
+
       <Flex direction="column" p={6} gap={6}>
         <Stat textAlign={{ base: 'center', md: 'left' }}>
           <StatLabel>Advanced Micro Devices (Singapore)</StatLabel>
